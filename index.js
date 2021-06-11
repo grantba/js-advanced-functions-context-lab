5ab03dfd43f2f7c45a104ea1f395bf042b30e5db
Blaire <grant.blaire@gmail.com>
2021-06-11 14:12:41 -0500

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,63 @@ let allWagesFor = function () {
 
     return payable
 }
+
+let wagesEarnedOnDate = function (stamp) {
+    const pay = this.payPerHour;
+    const hours = hoursWorkedOnDate.call(this, stamp);
+    return parseInt(pay * hours);
+}
+
+let calculatePayroll = function (array) {
+    let total = 0;
+    array.forEach(employee => {
+        employee.timeOutEvents.forEach(event => { 
+            total += wagesEarnedOnDate.call(employee, event.date)
+        })
+    })
+    return total;
+}
+
+let createEmployeeRecord = function (array) {
+    return  {firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function (array) {
+    return array.map(element => {
+        return createEmployeeRecord(element);
+    })
+}
+
+let createTimeInEvent = function (stamp) {
+    this.timeInEvents.push({type: "TimeIn", hour: parseInt(stamp.substr(-4)), date: stamp.substr(0, 10)})
+    return this;  
+}
+
+let createTimeOutEvent = function (stamp) {
+    this.timeOutEvents.push({type: "TimeOut", hour: parseInt(stamp.substr(-4)), date: stamp.substr(0, 10)})
+    return this;  
+}
+
+let hoursWorkedOnDate = function (stamp) {
+    const timeOutDate = this.timeOutEvents.find(element => {
+        return element.date === stamp;
+    });
+    const timeOut = timeOutDate.hour;
+    const timeInDate = this.timeInEvents.find(element => {
+        return element.date === stamp;
+    })
+    const timeIn = timeInDate.hour;
+    return (timeOut - timeIn) / 100;
+}
+
+let findEmployeeByFirstName = function (array, name) {
+    return array.find(person => {
+        return person.firstName === name;
+    })
+}
